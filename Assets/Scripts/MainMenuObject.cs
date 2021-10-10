e49669ec2f4e042ce76c10b14377b3010a61ab5e
Phillips Albright <55517109+phillipsalbright@users.noreply.github.com>
2021-10-09 23:20:13 -0400

    Fleshed out main menu

--- Assets/Scripts/MainMenuObject.cs
+++ Assets/Scripts/MainMenuObject.cs
@@ -4,20 +4,32 @@ using UnityEngine.SceneManagement;
 /**
  * This class is connected to a GameObject in the mainmenu scene and contains methods to be
  * called by attached buttons on the menu to load different scenes.
+ * Set the menuScreens and use the LoadScreen and LoadScene methods for buttons on the menus by
+ * setting them within the Unity Editor.
  */
 public class MainMenuObject : MonoBehaviour
 {
-    public void LoadMainMenu()
-    {
-        SceneManager.LoadScene(0);
-    }
+    public GameObject[] menuScreens;
+    
     public void QuitGame()
     {
         Application.Quit();
     }
 
-    public void Play()
+    public void LoadScreen(int screenIndex)
     {
-        SceneManager.LoadScene(1);
+        menuScreens[screenIndex].SetActive(true);
+        for (int i = 0; i < menuScreens.Length; i++)
+        {
+            if (menuScreens[i].activeInHierarchy == true && i != screenIndex)
+            {
+                menuScreens[i].SetActive(false);
+            }
+        }
+    }
+
+    public void LoadScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
     }
 }
